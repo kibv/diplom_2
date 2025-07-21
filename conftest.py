bc2a42329779f25e5419435408d30c7f43b8f85f
v.kib <vlad_kibkalo@mail.ru>
2025-07-21 13:17:12 +0300

    Revert "fix conftest"
    This reverts commit 0c1ab5ba322a79d453e1bf46ab6eacb848e936e8.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,40 @@
 import pytest
+from data.urls import REGISTER_URL, LOGIN_URL, USER_URL
+import requests
 from data.helpers import get_random
+# @pytest.fixture(scope="function")
+# def user_token():
+#     email = f"mail{get_random()}@yandex.ru"
+#     password = f"pass{get_random()}"
+#     name = f"name{get_random()}"
+#
+#     # Регистрация
+#     res = requests.post(REGISTER_URL, json={
+#         "email": email,
+#         "password": password,
+#         "name": name
+#     })
+#     assert res.status_code == 200
+#
+#     # Логин
+#     res_login = requests.post(LOGIN_URL, json={
+#         "email": email,
+#         "password": password
+#     })
+#     assert res_login.status_code == 200
+#     token = res_login.json()["accessToken"]
+#
+#     yield {
+#         "token": token,
+#         "email": email,
+#         "password": password
+#     }
+#
+#     # Удаление пользователя
+#     headers = {"Authorization": token}
+#     res_delete = requests.delete(USER_URL, headers=headers)
+#     assert res_delete.status_code in [200, 202]
+
 import requests
 from data.urls import REGISTER_URL, LOGIN_URL, USER_URL
 
